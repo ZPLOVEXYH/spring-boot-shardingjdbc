b15b00711cf0335ad42e0750e4c5e0fe809865b7
张鹏 <zhangpeng@samples.cn>
2021-01-27 16:22:06 +0800

    secord commit

---
+++ src/main/java/com/macky/springbootshardingjdbc/controller/BookController.java
@@ -0,0 +1,49 @@
+package com.macky.springbootshardingjdbc.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.macky.springbootshardingjdbc.entity.Book;
+import com.macky.springbootshardingjdbc.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+/**
+ * @author ZhangPeng
+ * @Title class BookController
+ * @Description: TODO
+ * @date 2019/7/12 20:53
+ */
+@RestController
+public class BookController {
+
+    @Autowired
+    BookService bookService;
+
+    @GetMapping(value = "/bookList")
+    public Object index(@Valid Book query,
+                        @RequestParam(value = "pageNum", defaultValue = "0") Integer pageNum,
+                        @RequestParam(value = "pageSize", defaultValue = "10") Integer pageSize) {
+
+        QueryWrapper<Book> wrapper = new QueryWrapper<>();
+        wrapper.lambda().orderByDesc(Book::getId);
+
+        // 查询第1页，每页返回10条
+        Page<Book> page = new Page<>(pageNum, pageSize);
+
+        return bookService.page(page, wrapper);
+    }
+
+    @RequestMapping(value = "/book", method = RequestMethod.GET)
+    public List<Book> getItems(String name) {
+        return bookService.getBookList(name);
+    }
+
+    @RequestMapping(value = "/book", method = RequestMethod.POST)
+    public Boolean saveItem(@RequestBody Book book) {
+        return bookService.save(book);
+    }
+}
